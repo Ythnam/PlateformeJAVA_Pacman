a7544bca859d8dde9a7546a18847bae7e984496e
phild <phild@DESKTOP-QI410AA>
2016-10-25 18:06:03 +0200

    Mise en place d'un singelton multithread pour Pacman avec le constructeur en private et la méthode getInstance

--- src/model/Pacman.java
+++ src/model/Pacman.java
@@ -11,10 +11,12 @@ import View.Field;
 
 public class Pacman implements Runnable{
 
+	private static Pacman instance; // Pacman est un singleton
+	
 	public static BufferedImage img;
 	private int x, y;
-	private double distance;
 	private Field field;
+	private boolean isPowerUp = false; // booleen pour le faire entrer ne état ou il bas les fantomes
 	
 	static{
 		try{
@@ -24,11 +26,28 @@ public class Pacman implements Runnable{
 		}
 	}
 	
-	public Pacman(int x, int y, Field field){
+	/*public Pacman(int x, int y, Field field){
+		this.x = x;
+		this.y = y;
+		this.field = field;
+	}*/
+	private Pacman(int x, int y, Field field){
 		this.x = x;
 		this.y = y;
 		this.field = field;
 	}
+	
+	
+	// Ce code pour le singleton a été récupéré sur le site suivant :
+	//http://christophej.developpez.com/tutoriel/java/singleton/multithread/
+	public static Pacman getInstance(int x, int y, Field field) {
+        if (null == instance) { // Premier appel
+            synchronized(Pacman.class) {
+                    instance = new Pacman(x, y, field);
+            }
+        }
+        return instance;
+    }
 	
 	@Override
 	public void run(){
@@ -36,7 +55,7 @@ public class Pacman implements Runnable{
 			while(true){
 				Thread.sleep(500);
 				
-				// Entrer le code pendant le RUn 
+				// Entrer le code pendant le RUn --> dépend des entrées clavier ici
 				
 				field.repaint();
 			}
