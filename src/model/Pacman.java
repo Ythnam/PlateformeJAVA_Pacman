fdf2ef99cb23f1a1b2f9055b65ae2d8d2c6787e8
Tiifred <fred.cordelois@hotmail.fr>
2016-10-29 09:50:03 +0000

    clean

--- src/model/Pacman.java
+++ src/model/Pacman.java
@@ -10,7 +10,7 @@ import javax.swing.ImageIcon;
 
 import View.Field;
 
-public class Pacman implements Runnable{
+public class Pacman {
 
 	private static Pacman instance; // Pacman est un singleton
 	
@@ -21,13 +21,7 @@ public class Pacman implements Runnable{
 	private Random rand = new Random(); // Sert juste pour les test tant qu'on n'a pas le clavier
 	public static ImageIcon imageIcon = new ImageIcon("image/pacman-haut.gif");
 	
-	/*static{
-		try{
-			img = ImageIO.read(new File("path"));
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}*/
+
 	
 	public static ImageIcon getImageIcon() {
 		return imageIcon;
@@ -67,42 +61,8 @@ public class Pacman implements Runnable{
 		this.field = field;
 	}
 	
-	//Fin du code pour le singleton
-	
-	@Override
-	public void run(){
-		try{
-			while(true){
-				Thread.sleep(500);
-				
-				tryToMove();
-				
-				field.repaint();
-			}
-		}  catch (InterruptedException e){
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Cette méthode permet de faire avancer le pacman en aléatoire
-	 */
-	private void tryToMove(){
-		int choice = rand.nextInt(4);
-		switch(choice){
-		case 0 : 
-			if(this.x+1 < this.field.getXMAX()) x++;
-			break;
-		case 1 :
-			if(this.y+1 < this.field.getYMAX()) y++;
-			break;
-		case 2 :
-			if(this.x > 0) x--;
-			break;
-		case 3 :
-			if(this.y > 0) y--;
-		}
-	}
+
+
 	public void goLeft(){
 		if(this.x+1 < this.field.getXMAX()) this.x--;
 	}
